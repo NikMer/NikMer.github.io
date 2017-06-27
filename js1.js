3050f84c2b52eccb6c51b1b862ec716846e52e8f
NikMer <nikmerzlikin@gmail.com>
2017-06-28 00:51:45 +0300

    Add files via upload

--- js1.js
+++ js1.js
@@ -1,3 +1,12 @@
 function cl1() {
-location.href = 'http://www.yandex.ru/';
+location.href = 'prog.html';
+}
+function cl2() {
+location.href = 'space.html';
+}
+function cl3() {
+location.href = 'sub.html';
+}
+function cl4() {
+location.href = 'index.html';
 }
