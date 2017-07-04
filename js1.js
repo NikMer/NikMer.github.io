8a69fdc0ae9ebf208390178eb601c6866394aabe
NikMer <nikmerzlikin@gmail.com>
2017-07-04 22:38:45 +0300

    Загружена версия 1_2_2

--- js1.js
+++ js1.js
@@ -10,3 +10,47 @@ location.href = 'sub.html';
 function cl4() {
 location.href = 'index.html';
 }
+
+//Проверка формы по полям
+function validate() {
+   //Считаем значения из полей Аккаутунт и E-mail в переменные x и y
+   var x=document.forms["form"]["account"].value;
+   var y=document.forms["form"]["email"].value;
+	 //Значения radiobuttons пихаем в свои переменные
+   var yes=document.getElementById("sx1");
+   var no=document.getElementById("sx2");
+   var bools=true;
+   
+   //Если поле Аккаутунт пустое выведем сообщение и предотвратим отправку формы
+   if ((x.length==0) || (y.length==0) || ((yes.checked==false)&&(no.checked==false))) {
+      document.getElementById("accountf").innerHTML="*";
+      document.getElementById("emailf").innerHTML="*";
+      document.getElementById("sxf").innerHTML="*";
+      alert("Заполните все поля");
+      bools=false;
+
+                     ga('send', {
+                       hitType: 'event',
+                       eventCategory: 'ProgrammerFields',
+                       eventAction: 'Не заполнены поля',
+                       eventLabel: x + ';' + y + ';'
+                     });            
+
+      return false;
+   }
+   //Если поле не содержит эти символы знач email введен не верно
+   if(new RegExp(/^[a-z0-9\!\#\$\%\&\'\*\+\/\=\?\^\_\`\{\|\}\~\-]+(?:\.[a-z0-9\.\!\#\$\%\&\'\*\+\/\=\?\^\_\`\{\|\}\~\-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[a-z]{2,6})$/i).test(y))
+	   {
+	   	bools=true;
+	   }
+			   else
+			   {
+			      document.getElementById("emailf").innerHTML="*";
+			      alert("E-mail введен неверно");
+			      bools=false;
+			      return false;
+			   }
+			   if (bools) {
+			   	alert('Форма отправлена!');
+			   }
+}
